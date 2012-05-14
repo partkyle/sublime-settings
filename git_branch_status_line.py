cb520020fc34f599c7683ae37e8d3a60ff38df0c
Kyle Partridge <kyle.partridge@sendgrid.com>
2012-05-14 14:00:34 -0700

    fix errors on non git repos with no folders

--- git_branch_status_line.py
+++ git_branch_status_line.py
@@ -4,6 +4,8 @@ import commands
 import os
 
 
+status_tag_name = '00_git_branch'
+
 # finds the current branch name for the dir param
 def git_branch_name(dir):
     command = 'cd "%s"; git symbolic-ref HEAD 2>/dev/null' % dir
@@ -26,8 +28,8 @@ class GitBranchStatusBarCommand(sublime_plugin.EventListener):
         self._update_status_line(view)
 
     # When a file is modified
-    def on_modified(self, view):
-        self._update_status_line(view)
+    # def on_modified(self, view):
+    #     self._update_status_line(view)
 
     # When sublime takes focus
     def on_activated(self, view):
@@ -35,20 +37,27 @@ class GitBranchStatusBarCommand(sublime_plugin.EventListener):
 
     # updates the status to the current branch
     def _update_status_line(self, view):
-        git_branch = git_branch_name(self._get_working_dir(view))
-        if git_branch != None and len(git_branch) > 0:
-            view.set_status('00-git_branch', format_git_branch(git_branch))
-        else:
-            view.erase_status('00-git_branch')
+        working_dir = self._get_working_dir(view)
+        view.erase_status(status_tag_name)
+        if working_dir != None and len(working_dir) > 0:
+            git_branch = git_branch_name(working_dir)
+            if len(git_branch) > 0:
+                view.set_status(status_tag_name, format_git_branch(git_branch))
 
     # If there is a file in the active view use that file's directory to
     # search for the Git root.  Otherwise, use the only folder that is
     # open.
     def _get_working_dir(self, view):
         file_name = view.file_name()
+        git_dir = None
         if file_name:
-            return os.path.dirname(file_name)
+            git_dir = os.path.dirname(file_name)
         elif view.window() != None and len(view.window().folders()) > 0:
-            return view.window().folders()[0]
+            git_dir = view.window().folders()[0]
         else:
+            git_dir = None
+
+        if git_dir == None:
             return None
+        elif os.path.exists(git_dir):
+            return git_dir
